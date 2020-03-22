99db7707d2663477e50bec9e8298452443169437
Arthur <arthur@jovart.com>
2020-03-22 19:44:51 +0100

    ✨ Upgrade the highcharts version

--- getbeaned/static/js/highcharts/dark-unica.src.js
+++ getbeaned/static/js/highcharts/dark-unica.src.js
@@ -1,5 +1,5 @@
 /**
- * @license Highcharts JS v7.0.3 (2019-02-06)
+ * @license Highcharts JS v8.0.2 (2020-03-03)
  *
  * (c) 2009-2019 Torstein Honsi
  *
@@ -11,23 +11,33 @@
         factory['default'] = factory;
         module.exports = factory;
     } else if (typeof define === 'function' && define.amd) {
-        define(function () {
+        define('highcharts/themes/dark-unica', ['highcharts'], function (Highcharts) {
+            factory(Highcharts);
+            factory.Highcharts = Highcharts;
             return factory;
         });
     } else {
         factory(typeof Highcharts !== 'undefined' ? Highcharts : undefined);
     }
 }(function (Highcharts) {
-    (function (Highcharts) {
-        /**
-         * (c) 2010-2019 Torstein Honsi
+    var _modules = Highcharts ? Highcharts._modules : {};
+    function _registerModule(obj, path, args, fn) {
+        if (!obj.hasOwnProperty(path)) {
+            obj[path] = fn.apply(null, args);
+        }
+    }
+    _registerModule(_modules, 'themes/dark-unica.js', [_modules['parts/Globals.js']], function (Highcharts) {
+        /* *
          *
-         * License: www.highcharts.com/license
+         *  (c) 2010-2020 Torstein Honsi
          *
-         * Dark theme for Highcharts JS
-         * @author Torstein Honsi
-         */
-
+         *  License: www.highcharts.com/license
+         *
+         *  Dark theme for Highcharts JS
+         *
+         *  !!!!!!! SOURCE GETS TRANSPILED BY TYPESCRIPT. EDIT TS FILE ONLY. !!!!!!!
+         *
+         * */
         /* global document */
         // Load the fonts
         Highcharts.createElement('link', {
@@ -35,7 +45,6 @@
             rel: 'stylesheet',
             type: 'text/css'
         }, null, document.getElementsByTagName('head')[0]);
-
         Highcharts.theme = {
             colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
                 '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
@@ -78,7 +87,6 @@
                 title: {
                     style: {
                         color: '#A0A0A3'
-
                     }
                 }
             },
@@ -108,7 +116,10 @@
             plotOptions: {
                 series: {
                     dataLabels: {
-                        color: '#B0B0B3'
+                        color: '#F0F0F3',
+                        style: {
+                            fontSize: '13px'
+                        }
                     },
                     marker: {
                         lineColor: '#333'
@@ -125,6 +136,7 @@
                 }
             },
             legend: {
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
                 itemStyle: {
                     color: '#E0E0E3'
                 },
@@ -133,6 +145,11 @@
                 },
                 itemHiddenStyle: {
                     color: '#606063'
+                },
+                title: {
+                    style: {
+                        color: '#C0C0C0'
+                    }
                 }
             },
             credits: {
@@ -145,7 +162,6 @@
                     color: '#707073'
                 }
             },
-
             drilldown: {
                 activeAxisLabelStyle: {
                     color: '#F0F0F3'
@@ -154,7 +170,6 @@
                     color: '#F0F0F3'
                 }
             },
-
             navigation: {
                 buttonOptions: {
                     symbolStroke: '#DDDDDD',
@@ -163,7 +178,6 @@
                     }
                 }
             },
-
             // scroll charts
             rangeSelector: {
                 buttonTheme: {
@@ -198,7 +212,6 @@
                     color: 'silver'
                 }
             },
-
             navigator: {
                 handles: {
                     backgroundColor: '#666',
@@ -214,7 +227,6 @@
                     gridLineColor: '#505053'
                 }
             },
-
             scrollbar: {
                 barBackgroundColor: '#808083',
                 barBorderColor: '#808083',
@@ -224,23 +236,14 @@
                 rifleColor: '#FFF',
                 trackBackgroundColor: '#404043',
                 trackBorderColor: '#404043'
-            },
-
-            // special colors for some of the
-            legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
-            background2: '#505053',
-            dataLabelsColor: '#B0B0B3',
-            textColor: '#C0C0C0',
-            contrastTextColor: '#F0F0F3',
-            maskColor: 'rgba(255,255,255,0.3)'
+            }
         };
-
         // Apply the theme
         Highcharts.setOptions(Highcharts.theme);
 
-    }(Highcharts));
-    return (function () {
+    });
+    _registerModule(_modules, 'masters/themes/dark-unica.src.js', [], function () {
 
 
-    }());
+    });
 }));
--- getbeaned/static/js/highcharts/modules/full-screen.src.js
+++ getbeaned/static/js/highcharts/modules/full-screen.src.js
@@ -1,5 +1,6 @@
 /**
- * @license Highcharts JS v7.0.3 (2019-02-06)
+ * @license Highstock JS v8.0.2 (2020-03-03)
+ *
  * Advanced Highstock tools
  *
  * (c) 2010-2019 Highsoft AS
@@ -13,56 +14,238 @@
         factory['default'] = factory;
         module.exports = factory;
     } else if (typeof define === 'function' && define.amd) {
-        define(function () {
+        define('highcharts/modules/full-screen', ['highcharts'], function (Highcharts) {
+            factory(Highcharts);
+            factory.Highcharts = Highcharts;
             return factory;
         });
     } else {
         factory(typeof Highcharts !== 'undefined' ? Highcharts : undefined);
     }
 }(function (Highcharts) {
-    (function (H) {
-        /**
-         * (c) 2009-2019 Sebastian Bochann
+    var _modules = Highcharts ? Highcharts._modules : {};
+    function _registerModule(obj, path, args, fn) {
+        if (!obj.hasOwnProperty(path)) {
+            obj[path] = fn.apply(null, args);
+        }
+    }
+    _registerModule(_modules, 'modules/full-screen.src.js', [_modules['parts/Globals.js']], function (H) {
+        /* *
+         * (c) 2009-2020 Rafal Sebestjanski
          *
          * Full screen for Highcharts
          *
          * License: www.highcharts.com/license
          */
-
-
-        H.FullScreen = function (container) {
-            this.init(container.parentNode); // main div of the chart
-        };
-
+        var addEvent = H.addEvent, Chart = H.Chart;
         /**
-         * The module allows user to enable full screen mode in StockTools.
+         * The module allows user to enable display chart in full screen mode.
+         * Used in StockTools too.
          * Based on default solutions in browsers.
          *
          */
-
-        H.FullScreen.prototype = {
-            /**
-             * Init function
+        /* eslint-disable no-invalid-this, valid-jsdoc */
+        /**
+         * Handles displaying chart's container in the fullscreen mode.
+         *
+         * @class
+         * @name Highcharts.Fullscreen
+         * @hideconstructor
+         * @requires modules/full-screen
+         */
+        var Fullscreen = /** @class */ (function () {
+            /* *
              *
-             * @param {HTMLDOMElement} - chart div
+             *  Constructors
              *
-             */
-            init: function (container) {
-                if (container.requestFullscreen) {
-                    container.requestFullscreen();
-                } else if (container.mozRequestFullScreen) {
-                    container.mozRequestFullScreen();
-                } else if (container.webkitRequestFullscreen) {
-                    container.webkitRequestFullscreen();
-                } else if (container.msRequestFullscreen) {
-                    container.msRequestFullscreen();
+             * */
+            function Fullscreen(chart) {
+                /**
+                 * Chart managed by the fullscreen controller.
+                 * @name Highcharts.Fullscreen#chart
+                 * @type {Highcharts.Chart}
+                 */
+                this.chart = chart;
+                /**
+                 * The flag is set to `true` when the chart is displayed in
+                 * the fullscreen mode.
+                 *
+                 * @name Highcharts.Fullscreen#isOpen
+                 * @type {boolean|undefined}
+                 * @since 8.0.1
+                 */
+                this.isOpen = false;
+                if (!(chart.container.parentNode instanceof Element)) {
+                    return;
+                }
+                var container = chart.container.parentNode;
+                // Hold event and methods available only for a current browser.
+                if (!this.browserProps) {
+                    if (typeof container.requestFullscreen === 'function') {
+                        this.browserProps = {
+                            fullscreenChange: 'fullscreenchange',
+                            requestFullscreen: 'requestFullscreen',
+                            exitFullscreen: 'exitFullscreen'
+                        };
+                    }
+                    else if (container.mozRequestFullScreen) {
+                        this.browserProps = {
+                            fullscreenChange: 'mozfullscreenchange',
+                            requestFullscreen: 'mozRequestFullScreen',
+                            exitFullscreen: 'mozCancelFullScreen'
+                        };
+                    }
+                    else if (container.webkitRequestFullScreen) {
+                        this.browserProps = {
+                            fullscreenChange: 'webkitfullscreenchange',
+                            requestFullscreen: 'webkitRequestFullScreen',
+                            exitFullscreen: 'webkitExitFullscreen'
+                        };
+                    }
+                    else if (container.msRequestFullscreen) {
+                        this.browserProps = {
+                            fullscreenChange: 'MSFullscreenChange',
+                            requestFullscreen: 'msRequestFullscreen',
+                            exitFullscreen: 'msExitFullscreen'
+                        };
+                    }
                 }
             }
-        };
+            /* *
+             *
+             *  Functions
+             *
+             * */
+            /**
+             * Stops displaying the chart in fullscreen mode.
+             * Exporting module required.
+             *
+             * @since       8.0.1
+             *
+             * @function    Highcharts.Fullscreen#close
+             * @return      {void}
+             * @requires    modules/full-screen
+             */
+            Fullscreen.prototype.close = function () {
+                var fullscreen = this, chart = fullscreen.chart;
+                // Don't fire exitFullscreen() when user exited using 'Escape' button.
+                if (fullscreen.isOpen &&
+                    fullscreen.browserProps &&
+                    chart.container.ownerDocument instanceof Document) {
+                    chart.container.ownerDocument[fullscreen.browserProps.exitFullscreen]();
+                }
+                // Unbind event as it's necessary only before exiting from fullscreen.
+                if (fullscreen.unbindFullscreenEvent) {
+                    fullscreen.unbindFullscreenEvent();
+                }
+                fullscreen.isOpen = false;
+                fullscreen.setButtonText();
+            };
+            /**
+             * Displays the chart in fullscreen mode.
+             * When fired customly by user before exporting context button is created,
+             * button's text will not be replaced - it's on the user side.
+             * Exporting module required.
+             *
+             * @since       8.0.1
+             *
+             * @function Highcharts.Fullscreen#open
+             * @return      {void}
+             * @requires    modules/full-screen
+             */
+            Fullscreen.prototype.open = function () {
+                var fullscreen = this, chart = fullscreen.chart;
+                // Handle exitFullscreen() method when user clicks 'Escape' button.
+                if (fullscreen.browserProps) {
+                    fullscreen.unbindFullscreenEvent = H.addEvent(chart.container.ownerDocument, // chart's document
+                    fullscreen.browserProps.fullscreenChange, function () {
+                        // Handle lack of async of browser's fullScreenChange event.
+                        if (fullscreen.isOpen) {
+                            fullscreen.isOpen = false;
+                            fullscreen.close();
+                        }
+                        else {
+                            fullscreen.isOpen = true;
+                            fullscreen.setButtonText();
+                        }
+                    });
+                    if (chart.container.parentNode instanceof Element) {
+                        var promise = chart.container.parentNode[fullscreen.browserProps.requestFullscreen]();
+                        if (promise) {
+                            promise['catch'](function () {
+                                alert(// eslint-disable-line no-alert
+                                'Full screen is not supported inside a frame.');
+                            });
+                        }
+                    }
+                    H.addEvent(chart, 'destroy', fullscreen.unbindFullscreenEvent);
+                }
+            };
+            /**
+             * Replaces the exporting context button's text when toogling the
+             * fullscreen mode.
+             *
+             * @private
+             *
+             * @since 8.0.1
+             *
+             * @requires modules/full-screen
+             * @return {void}
+             */
+            Fullscreen.prototype.setButtonText = function () {
+                var _a, _b, _c, _d;
+                var chart = this.chart, exportDivElements = chart.exportDivElements, exportingOptions = chart.options.exporting, menuItems = (_b = (_a = exportingOptions) === null || _a === void 0 ? void 0 : _a.buttons) === null || _b === void 0 ? void 0 : _b.contextButton.menuItems, lang = chart.options.lang;
+                if (((_c = exportingOptions) === null || _c === void 0 ? void 0 : _c.menuItemDefinitions) && ((_d = lang) === null || _d === void 0 ? void 0 : _d.exitFullscreen) &&
+                    lang.viewFullscreen &&
+                    menuItems &&
+                    exportDivElements &&
+                    exportDivElements.length) {
+                    exportDivElements[menuItems.indexOf('viewFullscreen')]
+                        .innerHTML = !this.isOpen ?
+                        (exportingOptions.menuItemDefinitions.viewFullscreen.text ||
+                            lang.viewFullscreen) : lang.exitFullscreen;
+                }
+            };
+            /**
+             * Toggles displaying the chart in fullscreen mode.
+             * By default, when the exporting module is enabled, a context button with
+             * a drop down menu in the upper right corner accesses this function.
+             * Exporting module required.
+             *
+             * @since 8.0.1
+             *
+             * @sample      highcharts/members/chart-togglefullscreen/
+             *              Toggle fullscreen mode from a HTML button
+             *
+             * @function Highcharts.Fullscreen#toggle
+             * @requires    modules/full-screen
+             */
+            Fullscreen.prototype.toggle = function () {
+                var fullscreen = this;
+                if (!fullscreen.isOpen) {
+                    fullscreen.open();
+                }
+                else {
+                    fullscreen.close();
+                }
+            };
+            return Fullscreen;
+        }());
+        H.Fullscreen = Fullscreen;
+        // Initialize fullscreen
+        addEvent(Chart, 'beforeRender', function () {
+            /**
+             * @name Highcharts.Chart#fullscreen
+             * @type {Highcharts.Fullscreen}
+             * @requires modules/full-screen
+             */
+            this.fullscreen = new H.Fullscreen(this);
+        });
 
-    }(Highcharts));
-    return (function () {
+        return H.Fullscreen;
+    });
+    _registerModule(_modules, 'masters/modules/full-screen.src.js', [], function () {
 
 
-    }());
+    });
 }));
